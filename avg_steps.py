813c6878486b12d5d3572d3624e2e6c21102729f
Anna <anna_dulevitz1@baylor.edu>
2022-09-08 22:28:31 -0500

    final change

--- avg_steps.py
+++ avg_steps.py
@@ -10,38 +10,44 @@ outfile = open('avg_steps.csv','w')
 csvfile = csv.reader(infile,delimiter=',')
 next(csvfile) #this skips the first line
 
-outfile.write("Month, Average Steps\n")
-
 
 #create variables
-months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+months = ['Month, Average Steps','January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 count = 0
 total = 0
-prev = 1
+prev = 0
 month = 0 
 average =0 
 
 
 #loop through one month at a time
 for line in csvfile:
+
     if int(line[0]) == prev: #change to while when it works
         count += 1 
         total += int(line[1])
     else:
-        while month<12:
+        if month<13:
+            count+=1
             average = total/count
             outfile.write(months[month])
             outfile.write(': ')
             outfile.write(str(average))
             outfile.write('\n')
-            total = 0
+            count=0
+            total = int(line[1])
             average = 0
             month+=1
-            prev = line[0]
-
-    #prev = line[0]
+            prev += 1
 
 
+
+count+=1
+average = total/count
+outfile.write(months[month])
+outfile.write(': ')
+outfile.write(str(average))
+outfile.write('\n')
 
 
 outfile.close()
